fd910b456d66ba078363a25ce1c51f9e04a8af2b
Superar <marcio.lima.inacio@gmail.com>
2017-04-18 14:51:02 -0300

    Implementação inicial de um tradutor com Keras

--- machine_translation/keras/tradutor.py
+++ machine_translation/keras/tradutor.py
@@ -3,6 +3,14 @@ import codecs
 import argparse
 import re
 from collections import Counter
+from keras.models import Sequential
+from keras.layers import GRU
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers.wrappers import TimeDistributed
+from keras.layers import RepeatVector
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-pt', '--path_pt', help='Caminho para texto em português',
@@ -11,6 +19,12 @@ parser.add_argument('-en', '--path_en', help='Caminho para texto em inglês',
                     type=str, default=None)
 parser.add_argument('-vsize', '--vocabulary_size', help='Tamanho do vocabulário',
                     type=int, default=50000)
+parser.add_argument('-esize', '--embedding_size', help='Dimensões para as word embeddings',
+                    type=int, default=64)
+parser.add_argument('-hsize', '--hidden_size', help='Dimensões para as redes neurais',
+                    type=int, default=512)
+parser.add_argument('-slen', '--sequence_length', help='Tamanho máximo para as sequências',
+                    type=int, default=128)
 
 FLAGS = parser.parse_args()
 
@@ -20,7 +34,7 @@ def read_data(path):
 
 
 def create_dataset(words):
-    count = [['UKN', -1]]
+    count = [['<IGN>', -1], ['<START>', -1], ['END', -1], ['UKN', -1]]
     count.extend(Counter(words).most_common(FLAGS.vocabulary_size - 1))
 
     dictionary = dict()
@@ -44,4 +58,31 @@ if not FLAGS.path_pt or not FLAGS.path_en:
 else:
     data_pt, dict_pt, rev_dict_pt = create_dataset(read_data(FLAGS.path_pt))
     data_en, dict_en, rev_dict_en = create_dataset(read_data(FLAGS.path_en))
-    print(data_pt, data_en)
+
+    hidden_size = 512
+
+    model = Sequential()
+    model.add(Embedding(FLAGS.vocabulary_size,
+                        FLAGS.embedding_size,
+                        input_length=FLAGS.sequence_length,
+                        mask_zero=True))
+    model.add(GRU(hidden_size,
+                  input_shape=(FLAGS.sequence_length, FLAGS.embedding_size)))
+    model.add(Dense(FLAGS.hidden_size,
+                    input_shape=(FLAGS.hidden_size,)))
+    model.add(Activation('relu'))
+    model.add(RepeatVector(FLAGS.sequence_length,
+                           input_shape=(None, FLAGS.hidden_size)))
+    model.add(GRU(FLAGS.hidden_size,
+                  input_shape=(None, FLAGS.hidden_size),
+                  return_sequences=True))
+    model.add(TimeDistributed(Dense(1, activation='softmax'),
+                              input_shape=(FLAGS.hidden_size, FLAGS.vocabulary_size)))
+    model.compile(optimizer='adam', loss='mse')
+
+    model.fit(data_pt, data_en,
+              batch_size=64, epochs=3)
+
+    model.reset_states()
+
+    scores = model.evaluate(data_pt, data_en)
