d275eb10a6da8714d885495d2cf2156b9388b3ad
Superar <marcio.lima.inacio@gmail.com>
2017-06-12 13:28:56 -0300

    Adição de mais camadas no modelo

--- machine_translation/keras/tradutor_backend.py
+++ machine_translation/keras/tradutor_backend.py
@@ -45,24 +45,43 @@ class Tradutor(object):
     def _build_model(self):
         """Constrói o modelo de rede neural."""
 
-        hidden_size = 512
+        hidden_size = self.options.hidden_size
 
         self.model.add(Embedding(self.options.vocabulary_size,
                             self.options.embedding_size,
                             input_length=self.options.sequence_length,
                             mask_zero=True))
         self.model.add(GRU(hidden_size,
-                      input_shape=(self.options.sequence_length, self.options.embedding_size)))
+                      input_shape=(self.options.sequence_length, self.options.embedding_size),
+                      return_sequences=True))
+        self.model.add(GRU(hidden_size,
+                      input_shape=(None, hidden_size),
+                      return_sequences=True))
+        self.model.add(GRU(hidden_size,
+                           input_shape=(None, hidden_size),
+                           return_sequences=True))
+        self.model.add(GRU(hidden_size,
+                           input_shape=(None, hidden_size)))
         self.model.add(Dense(self.options.hidden_size,
-                        input_shape=(None, self.options.hidden_size)))
+                        input_shape=(None, hidden_size)))
         self.model.add(Activation('relu'))
         self.model.add(RepeatVector(self.options.sequence_length,
                                input_shape=(None, self.options.hidden_size)))
         self.model.add(GRU(self.options.hidden_size,
                       input_shape=(None, self.options.hidden_size),
                       return_sequences=True))
+        self.model.add(GRU(self.options.hidden_size,
+                      input_shape=(None, self.options.hidden_size),
+                      return_sequences=True))
+        self.model.add(GRU(self.options.hidden_size,
+                      input_shape=(None, self.options.hidden_size),
+                      return_sequences=True))
+        self.model.add(GRU(self.options.hidden_size,
+                      input_shape=(None, self.options.hidden_size),
+                      return_sequences=True))
         self.model.add(TimeDistributed(Dense(1, activation='softmax'),
                                   input_shape=(self.options.sequence_length,1)))
+        # self.model.add(Activation('softmax'))
         self.model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
 
