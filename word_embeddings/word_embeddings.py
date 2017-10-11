9b93af2f2e0bd2dd2bd4b56f73be1f884570b2a7
Marcio Lima <marcio.lima.inacio@gmail.com>
2017-10-10 21:06:15 -0300

    Implementação de plot

--- word_embeddings/word_embeddings.py
+++ word_embeddings/word_embeddings.py
@@ -1,5 +1,8 @@
 from gensim.models.word2vec import Word2Vec
 from gensim.models.keyedvectors import KeyedVectors
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import os
 import subprocess
 
 
@@ -37,11 +40,25 @@ class WordEmbeddings(object):
     def load_word2vec(self, filename='word2vec_model.txt'):
         self.word2vec_model = KeyedVectors.load_word2vec_format(filename)
 
+    def plot(self, filename='word_embeddings.png', num_points=500):
+        plot_data = [self.word2vec_model[word]
+                     for word in self.word2vec_model.index2word[:num_points]]
+        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+        low_dim_data = tsne.fit_transform(plot_data)
 
-embedder = WordEmbeddings('../Corpus_FAPESP_pt-en_bitexts/fapesp-bitexts.pt-en.pt')
-embedder.process_sentences('../machine_translation/OpenNMT/tokenizer.perl')
-embedder.train_word2vec()
-embedder.save_Word2Vec('word2vec_model.txt')
+        plt.figure(figsize=(18, 18))
+        for i, word in enumerate(self.word2vec_model.index2word[:num_points]):
+            x, y = low_dim_data[i, :]
+            plt.scatter(x, y)
+            plt.annotate(word,
+                         xy=(x, y),
+                         xytext=(5, 2),
+                         textcoords='offset points',
+                         ha='right',
+                         va='bottom')
+        plt.savefig(filename)
+
+
 # embedder = WordEmbeddings()
 # embedder.process_sentences('../Corpus_FAPESP_pt-en_bitexts/fapesp-bitexts.pt-en.pt',
 #                            'tokenizer.perl')
@@ -49,3 +66,4 @@ embedder.save_Word2Vec('word2vec_model.txt')
 # embedder.save_word2vec()
 embedder = WordEmbeddings()
 embedder.load_word2vec()
+embedder.plot()
