255c2e65c512fafbefb272195969f69a008fcb80
Superar <marcio.lima.inacio@gmail.com>
2017-11-09 17:23:18 -0200

    Melhorias na estutura do projeto para genrealização e comentários em português

---
+++ word_embeddings/word2vec_embeddings.py
@@ -0,0 +1,92 @@
+# pylint: disable=C0111
+from word_embeddings import WordEmbeddings
+from gensim.models import Word2Vec
+from gensim.models.word2vec import KeyedVectors
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Word2VecModel(WordEmbeddings):
+    ''' Classe para operacoes com relacao ao modelo de word embeddings
+        word2vec. As embeddings sao calculadas a partir de ``src_sentences`'
+    '''
+
+    def __init__(self):
+        WordEmbeddings.__init__(self)
+        self.sentences = list()
+
+    def train(self, lang, corpus_path, dim=100):
+        ''' Treinamendo do modelo Word2Vec '''
+
+        self.sentences = self.process_sentences(lang, corpus_path)
+
+        model_word2vec = Word2Vec(self.sentences, size=dim)
+        # As embeddings serao salvas em ``modelo``
+        self.model = model_word2vec.wv
+        del model_word2vec
+
+    def save(self, filename='model_word2vec.txt'):
+        ''' Salva o modelo em ``filename`` '''
+
+        self.model.save_word2vec_format(filename)
+
+    def load(self, filename='model_word2vec.txt'):
+        ''' Carrega modelo salvo em ``filename`` '''
+
+        self.model = KeyedVectors.load_word2vec_format(filename)
+
+    def plot(self, filename='word_embeddings_word2vec.png', num_points=500):
+        ''' Traca o grafico com os vetores das palavras.
+        Uso o modelo t-SNE do modulo sklearn para projetar os vetores
+        para um espaco de 2 dimensoes. '''
+
+        # Usa t-SNE para fazer as projecoes
+        plot_data = [self.model[word]
+                     for word in self.model.index2word[:num_points]]
+        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+        low_dim_data = tsne.fit_transform(plot_data)
+
+        # Plot
+        plt.figure(figsize=(18, 18))
+        for i, word in enumerate(self.model.index2word[:num_points]):
+            (x, y) = low_dim_data[i, :] # pylint: disable=C0103
+            plt.scatter(x, y)
+            plt.annotate(word,
+                         xy=(x, y),
+                         xytext=(5, 2),
+                         textcoords='off points',
+                         ha='right',
+                         va='bottom')
+        plt.savefig(filename)
+
+    def plot_n_most_similar(self, word, num_neighbours=10,
+                            filename='word2vec_most_similar.png'):
+        ''' Traca o grafico com as n embeddings mais proximas de ``word``.
+        Usa a distancia de cosseno como medida do quao proximas sao as palavras.
+        '''
+
+        plot_data = [self.model[word]]
+        data_label = [word]
+
+        for (_word, _) in self.model.most_similar(positive=[word],
+                                                  topn=num_neighbours):
+            plot_data.append(self.model[_word])
+            data_label.append(_word)
+
+        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+        low_dim_data = tsne.fit_transform(plot_data)
+        ref = low_dim_data[0]
+        trans_low_dim_data = np.array([d - ref for d in low_dim_data])
+
+        # Plot
+        plt.figure(figsize=(18, 18))
+        for i, _word in enumerate(data_label):
+            (x, y) = trans_low_dim_data[i, :] # pylint: disable=C0103
+            plt.scatter(x, y)
+            plt.annotate(_word,
+                         xy=(x, y),
+                         xytext=(5, 2),
+                         textcoords='offset points',
+                         ha='right',
+                         va='bottom')
+        plt.savefig(filename)
--- word_embeddings/word_embeddings.py
+++ word_embeddings/word_embeddings.py
@@ -1,121 +1,49 @@
-from gensim.models.word2vec import Word2Vec
-from gensim.models.keyedvectors import KeyedVectors
-from sklearn.manifold import TSNE
-from nltk.tokenize.moses import MosesTokenizer
-import matplotlib.pyplot as plt
-import numpy as np
+# pylint: disable=C0111
 import string
+from nltk.tokenize.moses import MosesTokenizer
 
 
 class WordEmbeddings(object):
-    ''' Techniques for computing of word embeddings
+    ''' Tecnicas para computar word embeddings
 
-    sentences - Stores an list of lists.
-                Each sublist is a sentence for training '''
+    ``model`` - Modelo treinado
+    '''
 
     def __init__(self):
-        self.sentences = []
+        self.model = None
 
-    def process_sentences(self, corpus_path):
-        ''' Data preprocessing. Tokenization.
+    def process_sentences(self, lang, corpus_path):
+        ''' Pre-processamento dos dados. Toquenizacao.
 
-        corpus_path - Path to the text. One sentence per line
+        ``lang`` - Idioma para a tokenizacao.
+        ``corpus_path`` - Caminho para o texto. Uma sentenca por linha.
 
-        The tokenized sentences are stored at ``self.sentences`` in
-        the format of [['first', 'sentence'], ['second', 'sentence']]
+        As sentencas tokenizadas sao retornadas no formato:
+        [['primeira', 'sentenca'], ['segunda', 'sentenca']]
         '''
 
-        tokenizer = MosesTokenizer(lang='pt')
+        tokenized_sentences = list()
+
+        tokenizer = MosesTokenizer(lang=lang)
         table = str.maketrans('', '', string.punctuation)
 
         with open(corpus_path, 'r') as _file:
             for sent in _file:
-                # Lowercase and remove punctuation
+                # Lowercase e retira pontuacao
                 proc_sent = sent.lower().translate(table)
                 tok_sent = tokenizer.tokenize(proc_sent, return_str=True)
-                self.sentences.append(tok_sent.split())
+                tokenized_sentences.append(tok_sent.split())
 
-    def train_word2vec(self, dim=100):
-        ''' Training of the word2vec model '''
+        return tokenized_sentences
 
-        model = Word2Vec(self.sentences, size=100)
-        # The embeddings are stored at word2vec_model
-        self.word2vec_model = model.wv
-        del model
+    def train(self, lang1, corpus_path1, dim, lang2=None, corpus_path2=None):
+        raise NotImplementedError
 
-    def save_word2vec(self, filename='word2vec_model.txt'):
-        ''' Save model '''
+    def save(self, filename):
+        raise NotImplementedError
 
-        self.word2vec_model.save_word2vec_format(filename)
+    def load(self, filename):
+        raise NotImplementedError
 
-    def load_word2vec(self, filename='word2vec_model.txt'):
-        ''' Load model '''
-
-        self.word2vec_model = KeyedVectors.load_word2vec_format(filename)
-
-    def plot(self, filename='word_embeddings.png', num_points=500):
-        ''' Plot the embeddings.
-        This uses the t-SNE model from the sklearn model to project
-        the vectors to a 2-dimension space. '''
-
-        # Use t-SNE to do the projections
-        plot_data = [self.word2vec_model[word]
-                     for word in self.word2vec_model.index2word[:num_points]]
-        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-        low_dim_data = tsne.fit_transform(plot_data)
-
-        # Plot
-        plt.figure(figsize=(18, 18))
-        for i, word in enumerate(self.word2vec_model.index2word[:num_points]):
-            x, y = low_dim_data[i, :]
-            plt.scatter(x, y)
-            plt.annotate(word,
-                         xy=(x, y),
-                         xytext=(5, 2),
-                         textcoords='offset points',
-                         ha='right',
-                         va='bottom')
-        plt.savefig(filename)
-
-    def plot_n_most_similar(self, word, n=10,
-                            filename='word2vec_most_similar.png'):
-        ''' Plot the n most close embeddings from ``word``.
-        This uses the cossine distance as a measure of how close the words are.
-        '''
-
-        plot_data = [self.word2vec_model[word]]
-        data_label = [word]
-
-        for (word, _) in self.word2vec_model.most_similar(positive=[word],
-                                                          topn=n):
-            plot_data.append(self.word2vec_model[word])
-            data_label.append(word)
-
-        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-        low_dim_data = tsne.fit_transform(plot_data)
-        ref = low_dim_data[0]
-        trans_low_dim_data = np.array([d - ref for d in low_dim_data])
-
-        # Plot
-        plt.figure(figsize=(18, 18))
-        for i, word in enumerate(data_label):
-            x, y = trans_low_dim_data[i, :]
-            plt.scatter(x, y)
-            plt.annotate(word,
-                         xy=(x, y),
-                         xytext=(5, 2),
-                         textcoords='offset points',
-                         ha='right',
-                         va='bottom')
-        plt.savefig(filename)
-
-
-# embedder = WordEmbeddings()
-# embedder.process_sentences('../Corpus_FAPESP_pt-en_bitexts/fapesp-bitexts.pt-en.pt',
-#                            'tokenizer.perl')
-# embedder.train_word2vec()
-# embedder.save_word2vec()
-embedder = WordEmbeddings()
-embedder.load_word2vec()
-embedder.plot()
-print(MosesTokenizer(lang='pt').tokenize('Oi, tudo bem? Estou testando esse ``tokenizador\'\''))
+    def plot(self, filename, num_points):
+        raise NotImplementedError
