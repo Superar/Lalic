b7a12a0c0547a846f8b279741fc4fff0b1678b24
Marcio Lima <marcio.lima.inacio@gmail.com>
2018-04-12 16:03:03 -0300

    Script para filtrar apenas os erros anotados

---
+++ post_editing/filter.py
@@ -0,0 +1,24 @@
+from read_ape import ApeReader
+
+FILE_PATH = 'FAPESP_NMT_test-a_truecased_300_APE_lex-incTrWord'
+
+ape_reader = ApeReader(FILE_PATH)
+
+print('@annotations')
+print('-1')
+
+cores = list()
+for k in ape_reader.corrections:
+    flat = [sub[1] for sub in k]
+    cores.append(flat)
+
+for (i, x) in enumerate(cores):
+    if 'red' in x or 'green' in x or 'yellow' in x:
+        print(' '.join(ape_reader.src_lines[i]))
+        print(' '.join(ape_reader.ref_lines[i]))
+        print(' '.join(ape_reader.sys_lines[i]))
+        error_info = [','.join(map(str, e))
+                    for e in ape_reader.error_lines[i][:-1]]
+        error_info.append(ape_reader.error_lines[i][-1])
+        print('#'.join(error_info), end='')
+        print('#@'.join(['-.-'.join(w) for w in ape_reader.corrections[i]]))
